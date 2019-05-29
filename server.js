62bda7420755360cf11ebab6d64ea1683c11420a
Doug Sauve <dougsauve4.200@gmail.com>
2019-05-29 08:08:41 -0500

    set CORS policy to allow the front end 15

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 4205;
 
+const allowedUrl = (process.env.NODE_ENV = "production")
+? "https://git.heroku.com/secure-fortress-52545.git"
+: "http://localhost:4204";
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4204");
+  res.header("Access-Control-Allow-Origin", allowedUrl);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 
 app.get('/api/getWeather', (req, res) => {
 	const {
