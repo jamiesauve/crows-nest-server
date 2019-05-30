e681275dc9f8926242046001a7a91d2213d61aca
Doug Sauve <dougsauve4.200@gmail.com>
2019-05-30 15:09:38 -0500

    set up env vars
    10 min

--- server.js
+++ server.js
@@ -7,15 +7,12 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 4205;
 
-const allowedUrl = (process.env.NODE_ENV = "production")
-? "https://git.heroku.com/secure-fortress-52545.git"
-: "http://localhost:4204";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", allowedUrl);
+  res.header("Access-Control-Allow-Origin", process.env.ALLOWED_URL);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
